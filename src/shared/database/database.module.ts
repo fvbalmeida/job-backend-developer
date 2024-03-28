bb936d5be8e4ae4cc4af6a1662638f5bbbda9659
Flavio Vinicius <fvbalmeida20@gmail.com>
2024-03-28 16:16:29 -0300

    chore[config]: add database config

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow('MYSQL_HOST'),
+        port: configService.getOrThrow('MYSQL_PORT'),
+        database: configService.getOrThrow('MYSQL_DATABASE'),
+        username: configService.getOrThrow('MYSQL_USERNAME'),
+        password: configService.getOrThrow('MYSQL_PASSWORD'),
+        synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
+        migrations: [__dirname + '../../migrations/*{.ts,.js}'],
+        migrationsRun: true,
+        logging: true,
+        autoLoadEntities: true,
+        migrationsTableName: 'migrations_typeorm',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class DatabaseModule {}
