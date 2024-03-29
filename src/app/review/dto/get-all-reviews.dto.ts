12e6bce1e3145cc9af05f4a4d1761e9566d37904
Flavio Vinicius <fvbalmeida20@gmail.com>
2024-03-29 08:35:39 -0300

    feat[review]: add repository

---
+++ src/app/review/dto/get-all-reviews.dto.ts
@@ -0,0 +1,23 @@
+import { Transform } from 'class-transformer';
+import { IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class GetAllReviewsDto {
+  @IsNumber()
+  page: number = 1;
+
+  @IsNumber()
+  limit: number = 10;
+
+  @Transform(({ value }) => `movie.${value}`)
+  @IsOptional()
+  @IsIn(['movie.released', 'movie.imdbRating'])
+  sort?: 'released' | 'imdbRating';
+
+  @IsOptional()
+  @IsIn(['ASC', 'DESC'])
+  order?: 'ASC' | 'DESC' = 'DESC';
+
+  @IsOptional()
+  @IsString()
+  filter?: string = '';
+}
---
+++ src/app/review/repositories/review.repository.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+
+import { Movie } from '@/app/movie/entities/movie.entity';
+import { GetAllReviewsDto } from '../dto/get-all-reviews.dto';
+import { Review } from '../entities/review.entity';
+
+@Injectable()
+export class ReviewRepository extends Repository<Review> {
+  constructor(private dataSource: DataSource) {
+    super(Review, dataSource.createEntityManager());
+  }
+
+  async createReview(movie: Movie, reviewData: Review) {
+    return this.dataSource
+      .createEntityManager()
+      .transaction(async (manager) => {
+        const newMovie = await manager.save(movie);
+        reviewData.movie = newMovie;
+        const newReview = await manager.save(reviewData);
+        return newReview;
+      });
+  }
+
+  async userHasReviewedMovie(userId: number, imdbID: string) {
+    return this.dataSource
+      .createQueryBuilder(Review, 'review')
+      .leftJoinAndSelect('review.movie', 'movie')
+      .where('movie.imdbID = :imdbID', { imdbID })
+      .andWhere('review.UserId = :userId', { userId })
+      .getOne();
+  }
+
+  async getAllReviews(getAllReviewsDto: GetAllReviewsDto) {
+    const { page, limit, sort, order, filter } = getAllReviewsDto;
+    return this.dataSource
+      .createQueryBuilder(Review, 'review')
+      .leftJoinAndSelect('review.movie', 'movie')
+      .where(
+        'movie.title LIKE :filter OR movie.actors LIKE :filter OR movie.director LIKE :filter',
+        { filter: `%${filter}%` },
+      )
+      .orderBy(sort, order)
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getMany();
+  }
+
+  async getReviewById(userId: number, review_id: number) {
+    return this.dataSource
+      .createQueryBuilder(Review, 'review')
+      .leftJoinAndSelect('review.movie', 'movie')
+      .where('review.id = :review_id', { review_id })
+      .andWhere('review.userId = :userId', { userId })
+      .getOne();
+  }
+
+  async updateReview(
+    userId: number,
+    review_id: number,
+    review: Partial<Review>,
+  ) {
+    return this.dataSource
+      .createQueryBuilder(Review, 'review')
+      .update(Review)
+      .set(review)
+      .where('review.id = :review_id', { review_id })
+      .andWhere('review.userId = :userId', { userId })
+      .execute();
+  }
+
+  async deleteReview(userId: number, review_id: number) {
+    return this.dataSource
+      .createQueryBuilder(Review, 'review')
+      .delete()
+      .where('review.id = :review_id', { review_id })
+      .andWhere('review.userid = :userId', { userId })
+      .execute();
+  }
+}
