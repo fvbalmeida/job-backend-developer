b6725c7149baddf1a1e7862091b0f421a4c09cc0
Flavio Vinicius <fvbalmeida20@gmail.com>
2024-04-01 16:48:32 -0300

    test[auth]: add auth tests

---
+++ src/app/auth/auth.controller.spec.ts
@@ -0,0 +1,26 @@
+import { TestingModule, Test } from "@nestjs/testing"
+
+import { authServiceMock } from "@/shared/__mocks__/review.mock"
+import { AuthController } from "./auth.controller"
+
+describe("ReviewController", () => {
+  let authController: AuthController
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [authServiceMock],
+    }).compile()
+
+    authController = moduleFixture.get<AuthController>(AuthController)
+  })
+
+  it("Should be defined", () => {
+    expect(authController).toBeDefined()
+  })
+
+  it("Should login", async () => {
+    const result = await authController.login(authServiceMock)
+    expect(result).toEqual({ access_token: "mockToken" })
+  })
+})
